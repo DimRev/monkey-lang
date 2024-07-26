8afb41a339ada5a789afc9db33790a936fdbd75c
DimRev <dimrev444@gmail.com>
2024-07-26 16:54:41 +0300

    Add print to STDOUT fn, puts

--- evaluator/builtin.go
+++ evaluator/builtin.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/DimRev/monkey-lang/object"
+import (
+	"fmt"
+
+	"github.com/DimRev/monkey-lang/object"
+)
 
 var builtin = map[string]*object.Builtin{
 	"len": {
@@ -97,6 +101,14 @@ var builtin = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
