7accd7734d02b20737d142abae392717bfedeabe
DimRev <dimrev444@gmail.com>
2024-08-16 12:48:26 +0300

    Add to constant pool, add to intructions buffer

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -21,6 +21,37 @@ func New() *Complier {
 }
 
 func (c *Complier) Complie(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Complie(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Complie(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Complie(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Complie(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
 	return nil
 }
 
@@ -34,6 +65,24 @@ func (c *Complier) Bytecode() *Bytecode {
 		Instructions: c.instructions,
 		Constants:    c.constants,
 	}
+}
+
+func (c *Complier) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Complier) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
+}
+
+func (c *Complier) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+
+	return pos
 }
 
 func parse(input string) *ast.Program {
